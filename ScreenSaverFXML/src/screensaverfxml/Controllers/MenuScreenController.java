038280f4aa4d02e146858d87d39808f0cb7521b8
8300 <8300@8300-PC>
2019-05-31 15:59:02 +0200

    Saving photos point

--- ScreenSaverFXML/src/screensaverfxml/Controllers/MenuScreenController.java
+++ ScreenSaverFXML/src/screensaverfxml/Controllers/MenuScreenController.java
@@ -5,34 +5,18 @@
  */
 package screensaverfxml.Controllers;
 
-import java.awt.Desktop;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.util.List;
 import javafx.application.Platform;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.geometry.Point3D;
-import javafx.geometry.Rectangle2D;
-import javafx.scene.Node;
-import javafx.scene.SnapshotParameters;
-import javafx.scene.canvas.Canvas;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.StackPane;
-import javafx.scene.transform.NonInvertibleTransformException;
-import javafx.scene.transform.Rotate;
-import javafx.scene.transform.Transform;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 /**
@@ -43,11 +27,12 @@ public class MenuScreenController{
  
     @FXML
     private StackPane stackImgPane;
-   @FXML
-   ImageView imgFieldView;
-   int photoSwipCounter = 0;
-   Image image;
-      
+    @FXML
+    ImageView imgFieldView;
+    int photoSwipCounter = 0;
+    Image image;
+    double angleRotation;
+
 //   private Desktop desktop = Desktop.getDesktop();
    private MainScreenController mainScreenController;
    final FileChooser fileChooser = new FileChooser();
@@ -64,6 +49,7 @@ public class MenuScreenController{
     List<File> selectedImgsList;
     File singleFile;
     
+    //Method to make image on the center of the screen
     public void centerImage(ImageView imgView) {
         Image img = imgView.getImage();
         if (img != null) {
@@ -72,6 +58,7 @@ public class MenuScreenController{
             double ratioX = imgView.getFitHeight() / img.getWidth();
             double ratioY = imgView.getFitWidth() / img.getHeight();
             double reducCoeff = 0;
+            
             if (ratioX >= ratioY) {
                 reducCoeff = ratioY;
             } else {
@@ -84,14 +71,17 @@ public class MenuScreenController{
         }
     }
     
+    //Select photos from driver by double click on imageView field
     @FXML
     public void imgDoubleClick(MouseEvent mouseEvent) throws MalformedURLException {
 
         // image view setting position
-        centerImage(imgFieldView);
+//        centerImage(imgFieldView);
         // end of image view setting position
         imgFieldView.requestFocus();
-
+        /**
+         * Code responsible for load photos from a driver
+        */
         if (mouseEvent.getButton().equals(MouseButton.PRIMARY)) {
             if (mouseEvent.getClickCount() == 2) {
                 fileChooser.getExtensionFilters().addAll(new ExtensionFilter("Img Files", "*.jpg", "*.jpeg", "*.png"));
@@ -109,16 +99,27 @@ public class MenuScreenController{
                 } else {
                     System.out.println("No File Selected");
                 }
+                /**
+                 * Code responsible for choose save destination folder
+                 */
+                
+                
+                
+                
             }
         }
     }
     
+    
     @FXML
     public void getNewImageHandler(KeyEvent event) {
+        /**
+         * Code responsible for switching photos
+         */
         centerImage(imgFieldView);
         imgFieldView.requestFocus();
         System.out.println(singleFile.getName());
-        
+
         if (event.getCode().equals(KeyCode.N)) {
             photoSwipCounter++;
             System.out.println("N clicked");
@@ -140,125 +141,35 @@ public class MenuScreenController{
                 900, 400,
                 true, true, true);
         imgFieldView.setImage(image);
+        //---***---***---***---***---***---***---***---***---***---***---***---*
         
-        /*
-        from imageRotation()
-        */
-        
-        Rectangle2D viewPoint = imgFieldView.getViewport();
-        Rotate rotation = new Rotate();
-        
+        /**
+         * Code responsible for rotate the image
+         */
         if (event.getCode().equals(KeyCode.L)) {
-            
-            SnapshotParameters params = new SnapshotParameters();
-            Canvas canvas = new Canvas(100,100);
-            
-            image = imgFieldView.snapshot(params, null);
-            canvas.getGraphicsContext2D().drawImage(image, 0, 0);
-            //dokonczyc z https://stackoverflow.com/questions/40059836/rotating-image-in-javafx
-            
-//            stackImgPane.setBackground(new Background(new BackgroundImage(
-//                    imgFieldView.getImage(), BackgroundRepeat.NO_REPEAT,
-//                    BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
-//                    BackgroundSize.DEFAULT)));
-            
-            
-//            stackImgPane.getTransforms().add(new Rotate(90));
-            
-//            imgFieldView.setRotationAxis(Point3D.ZERO);
-            
-//            rotation.setAngle(90);
-//            rotation.setPivotX(imgFieldView.getImage().getHeight()/2);
-//            rotation.setPivotY(imgFieldView.getImage().getWidth()/2);
 
-
-//            imgFieldView.getTransforms().add(new Rotate(90, Rotate.Z_AXIS));
-           
-//              addRotate(stackImgPane, Rotate.Z_AXIS, 45);
-
-            
-//            rotation.setPivotX(imgFieldView.getRotate());
-            
-            imgFieldView.getLocalToSceneTransform();
-//            imgFieldView.setFitHeight(400);
-//            imgFieldView.setFitWidth(900);
-            imgFieldView.setPreserveRatio(true);
-//            rotation.setPivotX(imgFieldView.getFitWidth() /2);
-//            rotation.setPivotY(imgFieldView.getFitHeight() /2);    
-//            rotation.setPivotY(imgFieldView.getImage().getRequestedHeight() /2);
-//            rotation.setPivotX(imgFieldView.getImage().getRequestedWidth() /2);
-            
-//            rotation.setPivotX(viewPoint.getMaxX() /2);
-//            rotation.setPivotY(viewPoint.getMaxY()/2);
-//            rotation.setAngle(rotation.getAngle() + 90);
-
-//            imgFieldView.getTransforms().add(new Rotate(90, imgFieldView.getFitHeight(), imgFieldView.getFitWidth()));
-//            imgFieldView.getTransforms().add(new Rotate(90, imgFieldView.getRotationAxis()));
-            imgFieldView.getTransforms().add(new Rotate(90,
-                    imgFieldView.getBoundsInParent().getMinX()
-                    + (imgFieldView.getBoundsInLocal().getWidth() / 2),
-                    imgFieldView.getBoundsInParent().getMinY()
-                    + (imgFieldView.getBoundsInLocal().getHeight() / 2)));
-
-//            imgFieldView.getTransforms().add(new Rotate(90,
-//                    imgFieldView.getFitWidth()
-//                    - (imgFieldView.getImage().getWidth() / 2),
-//                    imgFieldView.getFitHeight()
-//                    - (imgFieldView.getImage().getHeight() / 2)));
-
-//            imgFieldView.getTransforms().add(new Rotate(90, imgFieldView.getFitWidth()/2, imgFieldView.getFitHeight()/2));
-//            imgFieldView.getTransforms().add(new Rotate(90, image.getRequestedWidth(), image.getRequestedHeight()));
-            
-            
-            
-//            imgFieldView.setRotate(90);
-//            rotation.setPivotX(imgFieldView.getRotate());
-            imgFieldView.getTransforms().add(rotation);
-//            imgFieldView.getTransforms().add(new Rotate(90, imgFieldView.getFitHeight(), imgFieldView.getFitWidth()));
-//            viewPoint = imgFieldView.getViewport();
-            System.out.println("L clicked");
-        }
-        
-        if (event.getCode().equals(KeyCode.R)) {
-            imgFieldView.setRotate(-90);
+            angleRotation += 90;
+            imgFieldView.setRotate(angleRotation);
             imgFieldView.getViewport();
-            System.out.println("R clicked");
         }
+
+        if (event.getCode().equals(KeyCode.R)) {
+            angleRotation -= 90;
+            imgFieldView.setRotate(angleRotation);
+            imgFieldView.getViewport();
+        }
+        //---***---***---***---***---***---***---***---***---***---***---***---*
+        
+        
     }
     
-//    public static void addRotate(Node node, Point3D rotationAxis, double angle) {
-//    ObservableList<Transform> transforms = node.getTransforms();
-//    try {
-//        for (Transform t : transforms) {
-//            rotationAxis = t.inverseDeltaTransform(rotationAxis);
-//        }
-//    } catch (NonInvertibleTransformException ex) {
-//        throw new IllegalStateException(ex);
-//    }
-//    transforms.add(new Rotate(angle, rotationAxis));
-//}
     
     @FXML
-    public void imageRotation(KeyEvent event) {
-        if (event.getCode().equals(KeyCode.L)) {
-            imgFieldView.setRotate(90);
-            System.out.println("L clicked");
-
-        }
-        if (event.getCode().equals(KeyCode.R)) {
-            imgFieldView.setRotate(-90);
-            System.out.println("R clicked");
-        }
-    }
-    
-    @FXML 
     public void savePhoto(File singleFile) {
         File fileDirectoryOne = fileChooser.getInitialDirectory();
-        
+
         File fileDirectory = new File(System.getProperty(fileChooser.getInitialDirectory().toURI().toString()));
-        
-        
-        
+
     }
 
 
